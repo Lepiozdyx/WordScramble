c020ff88b32f4f97fbcc3966870a5a5190bf79d5
Alex Zhulai <Lepiozdyx@gmail.com>
2023-10-31 14:19:28 -0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Alex on 31.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
